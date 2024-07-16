89011f560f4fb1e26a71f0111c96d48219b1cb6b
JangLeeJun <dlwns0784@gmail.com>
2024-07-16 12:00:15 +0900

    "/" 라우팅용 IndexController 추가

---
+++ src/main/java/com/ictedu/springgit/indexController.java
@@ -0,0 +1,11 @@
+package com.ictedu.springgit;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class indexController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
